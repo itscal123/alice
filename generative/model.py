06bbbd2ec0770f968138b347d61d633b500eadf3
Calvin Lee <calvin.kory@gmail.com>
2022-02-03 18:27:55 -0800

    rename file

---
+++ generative/model.py
@@ -0,0 +1,23 @@
+# TODO: Load trained model
+# TODO: Create a model class to handle user inputs
+# TODO: Create a model class to output encoder-decoder outputs (response)
+# TODO: Communicate how to format the retrieval/generative responses
+
+class Generative():
+    def __init__(self):
+        """
+        TODO: Load the trained model
+        """
+        return
+
+    def userInput(self):
+        """
+        TODO: Takes some sort of user input and feeds it to the generative model.
+        TODO: Likely takes in input from some sort of Interface class which handles
+              all user input preprocessing/formatting
+        TODO: Save response and retrieve using getResponse???
+        """
+        return
+
+    def getResponse(self):
+        return

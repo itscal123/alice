a71ca9fc1a5c2be159c069aef4e662e1cd29dc16
Calvin Lee <calvin.kory@gmail.com>
2022-02-05 23:06:21 -0800

    Update model script

--- generative/model.py
+++ generative/model.py
@@ -1,23 +1,71 @@
-# TODO: Load trained model
-# TODO: Create a model class to handle user inputs
-# TODO: Create a model class to output encoder-decoder outputs (response)
-# TODO: Communicate how to format the retrieval/generative responses
+from __future__ import unicode_literals
+
+import torch
+import pickle
+from train import Encoder, Decoder, GreedySearchDecoder, GlobalAttn
+from data import Voc, normalizeString, indexesFromSentence, Data
+import unicodedata
+import codecs
 
 class Generative():
     def __init__(self):
-        """
-        TODO: Load the trained model
-        """
-        return
+        self.encoder = torch.load("generative\models\encoder.pt")
+        self.decoder = torch.load("generative\models\decoder.pt")
+        self.searcher = torch.load("generative\models\searcher.pt")
+        data = pickle.load(open("generative\data.p", "rb"))
+        voc, pairs, save_dir, corpus_name = data.loadData()
+        self.voc = voc
+        USE_CUDA = torch.cuda.is_available()
+        self.device = torch.device("cuda" if USE_CUDA else "cpu")
 
-    def userInput(self):
-        """
-        TODO: Takes some sort of user input and feeds it to the generative model.
-        TODO: Likely takes in input from some sort of Interface class which handles
-              all user input preprocessing/formatting
-        TODO: Save response and retrieve using getResponse???
-        """
-        return
 
-    def getResponse(self):
-        return
+    def evaluate(self, sentence, max_length=10):
+        # Format input sentence as a batch
+        # words -> indexes
+        indexes_batch = [indexesFromSentence(self.voc, sentence)]
+
+        # Create lengths tensor
+        lengths = torch.tensor([len(indexes) for indexes in indexes_batch])
+
+        # Transpose dimensions of batch to match models' expectations
+        input_batch = torch.LongTensor(indexes_batch).transpose(0,1)
+        
+        # Use appropriate device
+        input_batch = input_batch.to(self.device)
+        lengths = lengths.to("cpu")
+
+        # Decoder sentence with searcher
+        tokens, scores = self.searcher(input_batch, lengths, max_length)
+
+        # Indexes -> words
+        decoded_words = [self.voc.index2word[token.item()] for token in tokens]
+        return decoded_words
+
+    def run(self):
+        input_sentence = ""
+        while(True):
+            try:
+                # Get input sentence
+                input_sentence = input("User: ")
+
+                # Check if it is quit case
+                if input_sentence == "q" or input_sentence == "quit":
+                    break
+
+                # Normalize sentence
+                input_sentence = normalizeString(input_sentence)
+
+                # Evaluate sentence
+                output_words = self.evaluate(input_sentence)
+
+                # Format and print reponse
+                output_words[:] = [x for x in output_words if not (x == "EOS" or x == "PAD")]
+                print("ALICE:", " ".join(output_words))
+            
+            except KeyError as e:
+                print("Sorry I've never heard the word {} before.".format(e.args[0]))
+        print("ALICE: See you next time!")
+
+if __name__ == "__main__":
+    model = Generative()
+    model.run()

48c2efe47042516de2d1c9530c1f6a693bdb89ae
Calvin Lee <calvin.kory@gmail.com>
2022-02-20 12:48:21 -0800

    Load the generative model

--- interface.py
+++ interface.py
@@ -1,3 +1,12 @@
 # TODO: Interface class that should handle user inputs and model outputs
-# TODO: Load the generative model class
 # TODO: Load the IR model class
+import sys
+sys.path.insert(1, "generative\\")  # Add generative folder to path
+
+# Generative model imports
+from model import Generative
+from train import Encoder, Decoder, GreedySearchDecoder, GlobalAttn, BeamSearchDecoder
+from data import Voc, normalizeString, indexesFromSentence, Data
+
+if __name__ == "__main__":
+    generative = Generative()
